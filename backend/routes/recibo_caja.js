41139716004916105f5e8b40e6e5b34b72edd12d
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-07-25 00:01:13 -0500

    feat: :sparkles: GET Recibo de caja
    Obtencion de toda la información relevante repecto a la  tabla

--- backend/routes/recibo_caja.js
+++ backend/routes/recibo_caja.js
@@ -17,7 +17,23 @@ router_Recibo_caja.use((req,res,next)=>{
 
 router_Recibo_caja.get("/", proxy_recibo_cajas ,(req,res)=>{
     con.query(
-        `SELECT * FROM Recibo_caja;`,
+        `SELECT 
+        Recibo_caja.recibo_caja_id as id,
+        Pago.pago_id as id_pago,
+        Persona.persona_nombre as name,
+        Persona.persona_phone as phone,
+        Persona.persona_email as email,
+        Ubicacion.ubicacion_direccion as direction,
+        Pago.monto_num as monto,
+        Pago.valor_unitario as unidad,
+        Pago.subtotal_por_item as subtotal_por_item,
+        Pago.total as total,
+        Metodo_pago.mp_nombre as metodo_pago
+        FROM Recibo_caja
+        INNER JOIN Persona ON Persona.persona_id = Recibo_caja.persona_id
+        INNER JOIN Pago ON Pago.pago_id = Recibo_caja.pago_id
+        INNER JOIN Ubicacion ON Ubicacion.ubicacion_id = Persona.ubicacion_id
+        INNER JOIN Metodo_pago ON Metodo_pago.metodo_pago_id = Pago.metodo_pago_id;`,
         (err,data,fill)=>{
             if(err){
                 console.error('Error al obtener los datos: ', err.message);
