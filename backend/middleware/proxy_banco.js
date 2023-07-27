5bfdb7dbe9e361799dc92b43acd0f6b68228a67c
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-07-26 21:52:40 -0500

    feat: :bug: Correcion del callback
    Se corrigió el llamado a la clase del export get_bancos

--- backend/middleware/proxy_banco.js
+++ backend/middleware/proxy_banco.js
@@ -1,13 +1,13 @@
 import express from "express"; 
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
-import { get_banco } from '../controllers/get_banco.js';
+import { get_bancos } from '../controllers/get_bancos.js';
 import { validate } from "class-validator";
 
 const proxy_banco = express();
 proxy_banco.use(async (req,res,next)=>{
     try {
-        let data = plainToClass(get_banco, req.body, { excludeExtraneousValues: true});
+        let data = plainToClass(get_bancos, req.body, { excludeExtraneousValues: true});
         await validate(data);
         next();
     } catch (err) {
