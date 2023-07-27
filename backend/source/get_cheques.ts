bc37506fad01ec1bd76abd014e0a2906471ad87a
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-07-26 21:46:40 -0500

    feat: :safety_vest: Validaciones de las nuevas columnas
    se implementó la validación de las nuevas columnas en el archivo get_cheques.ts

--- backend/source/get_cheques.ts
+++ backend/source/get_cheques.ts
@@ -28,8 +28,36 @@ export class get_cheques {
     {toClassOnly: true})
     pagoID: Number;
 
-    constructor(personaID: Number, pagoID: Number){
-        this.personaID = personaID;
-        this.pagoID = pagoID;
+    @Expose ({name : 'banco_emisor_id'})
+    @Transform(({value})=>{
+        if(!Math.floor(value) && value > 0){
+            throw {
+                status: 400,
+                message: `Para el identificador del pago, se solicita que solo sea de tipo numérico.`
+            };
+        }
+        return value;
+    },
+    {toClassOnly: true})
+    emisor: Number;
+
+    @Expose ({name : 'banco_receptor_id'})
+    @Transform(({value})=>{
+        if(!Math.floor(value) && value > 0){
+            throw {
+                status: 400,
+                message: `Para el identificador del pago, se solicita que solo sea de tipo numérico.`
+            };
+        }
+        return value;
+    },
+    {toClassOnly: true})
+    receptor: Number;
+
+    constructor(personaID: Number, pagoID: Number,emisor: Number,receptor: Number){
+        this.personaID = personaID
+        this.pagoID = pagoID
+        this.emisor = emisor
+        this.receptor = receptor
     }
 }
