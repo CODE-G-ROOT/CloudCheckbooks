5695656ac5ee23fb636272339ed6f275528c71c7
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-07-26 21:27:04 -0500

    feat: :safety_vest: Validación tabla Banco
    Se implementó el codigo que valida a la tabla de banco

---
+++ backend/source/get_bancos.ts
@@ -0,0 +1,22 @@
+import {Expose, Transform} from 'class-transformer';
+
+export class get_fechas {
+
+    @Expose ({name : 'banco_name'})
+    @Transform (({value})=>{
+        if(value){
+            throw {
+                status: 400,
+                message: `Los terminos y condiciones solo pueden ser de tipo string.`
+            }
+        }
+        return value
+    },
+    {toClassOnly: true})
+    banco : String;
+
+    constructor  (banco : String){
+        this.banco = banco;
+    }
+}
+
